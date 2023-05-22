16cb31a11f4da8ef35439ba408232fb0baeb11e6
Ajithraina3348 <134273942+Ajithraina3348@users.noreply.github.com>
2023-05-22 15:01:33 +0530

    Update streamlit_app.py
    Added the menu item symbols

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,8 @@ import streamlit;
 streamlit.title('My Parents New Healthy Diner');
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Otameal')
-streamlit.text('Kale, Spianch & Rocket Smoothie')
-streamlit.text('Hard_Boiled Free-Range Eggs')
+streamlit.text('🥣 Omega 3 & Blueberry Otameal')
+streamlit.text('🥗 Kale, Spianch & Rocket Smoothie')
+streamlit.text('🐔 Hard_Boiled Free-Range Eggs')
+streamlit.text('🥑🍞 Avocado Toast')
+
